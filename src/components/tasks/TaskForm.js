289c76bc6de69a8fa3dfe0954e65bc0d20025af6
Zach Nicholson <raiste.doctow@gmail.com>
2020-06-11 10:35:32 -0500

    tasks finished, edit btn works, put works, checking userId correctly

--- src/components/tasks/TaskForm.js
+++ src/components/tasks/TaskForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ApiManager from '../../modules/ApiManager'
 
 const TaskForm = props => {
@@ -7,21 +7,34 @@ const TaskForm = props => {
     const handleFieldChange = e => {
         const stateToChange = {...task}
         stateToChange[e.target.id] = e.target.value
+        if (props.isEdit) {
+            stateToChange.id = parseInt(props.match.params.taskId)
+        }
         setTask(stateToChange)
+        console.log(props)
     }
 
     const handleSubmit = e => {
         e.preventDefault()
         if (props.isEdit) {
             ApiManager.update('tasks', task)
+                .then(() => props.history.push('/tasks'))
         } else {
             ApiManager.post('tasks', task)
+                .then(() => props.history.push('/tasks'))
         }
     }
 
     const handleDiscard = e => {
         props.history.push('/tasks')
     }
+
+    useEffect(() => {
+        if (props.match.params.taskId) {
+            ApiManager.get('tasks', props.match.params.taskId)
+                .then(setTask)
+        }
+    }, [props.match.params.taskId])
 
     return (
         <form className="task_form">
--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -3,32 +3,33 @@ import TaskCard from './TaskCard'
 import ApiManager from '../../modules/ApiManager'
 
 const TaskList = props => {
-    const [isComplete, setIsComplete] = useState([false])
+    const [isCurrent, setIsCurrent] = useState([true])
     const [tasks, setTasks] = useState([])
-
     
-    const handleToggle = e => setIsComplete(!isComplete)
+    const handleToggle = e => setIsCurrent(!isCurrent)
 
     const completeTask = task => {
         ApiManager.update('tasks', task)
-            .then(() => setIsComplete(true))
+            .then(() => setIsCurrent(false))
     }
 
     const deleteTask = id => {
         ApiManager.delete('tasks', id)
-            .then(() => setIsComplete(true))
+            .then(() => setIsCurrent(true))
     }
 
     useEffect(() => {
-        const getTasks = () => ApiManager.getByProperty('tasks', 'complete', !isComplete)
-        getTasks().then(data => setTasks(data))
-    }, [isComplete])
+        const getTasks = () => ApiManager.getByProperty('tasks', 'userId', JSON.parse(sessionStorage.user).id)
+        getTasks().then(data => {
+            setTasks(data.filter(each => each.complete === !isCurrent))
+        })
+    }, [isCurrent])
 
         return (
             <section className="task_list">
                 <button type="button" onClick={() => props.history.push('/tasks/add')}>Add Task</button>
-                {isComplete ? <button type="button">Current</button> : <button type="button" onClick={handleToggle}>Current</button>}
-                {isComplete ? <button type="button" onClick={handleToggle}>Completed</button> : <button type="button">Completed</button>}
+                {isCurrent ? <button type="button">Current</button> : <button type="button" onClick={handleToggle}>Current</button>}
+                {isCurrent ? <button type="button" onClick={handleToggle}>Completed</button> : <button type="button">Completed</button>}
                 {tasks.map(task => <TaskCard key={task.id} id={task.id} task={task} completeTask={completeTask} deleteTask={deleteTask} {...props} />)}
             </section>
         )
